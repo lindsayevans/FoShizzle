30b81762d0e96e2bd0cdd4c4931dbba8699fbe7c
Lindsay Evans <linz@linz.id.au>
2010-09-30 07:41:24 +1000

    Adding unit tests for resolution; fixing syntax errors

--- fo-shizzle.js
+++ fo-shizzle.js
@@ -229,15 +229,15 @@
 			return test_number(p, e, screen.height);
 		},
 
-		test_feature_resolution = function(p, e){}
+		test_feature_resolution = function(p, e){
 
 			var head = document.getElementsByTagName('head')[0],
 					body = document.getElementsByTagName('body')[0],
 					style = document.createElement('style'),
 					test = document.createElement('div'),
-					units = e.indexOf('dpi' === -1 ? 'cm' : 'in',
+					units = e !== null && e.indexOf('dpi' === -1) ? 'cm' : 'in',
 					width = units === 'in' ? '2.54' : '1',
-					style_content = '#' + FoShizzle.test_id_prefix + 'dpi-test { width: ' + width + 'cm !important; padding: 0 !important; } }'
+					style_content = '#' + FoShizzle.test_id_prefix + 'dpi-test { width: ' + width + 'cm !important; padding: 0 !important; } }',
 					ppu;
 
 			style.setAttribute('id', FoShizzle.test_id_prefix + 'dpi-test-style');
@@ -273,7 +273,7 @@
 			'color': test_feature_unimplemented,
 			'color-index': test_feature_unimplemented,
 			'monochrome': test_feature_unimplemented,
-			'resolution': test_feature_resolution, 
+			'resolution': test_feature_resolution,
 			'scan': test_feature_unimplemented,
 			'grid': test_feature_unimplemented
 		},
